bf70ca9395a601a4ba0d7e456dbad6deac6675a7
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2024-12-24 18:50:11 -0500

    feat: Add nav bar to navigate in app

--- src/app/layout/content/components/nav-bar/nav-bar.component.ts
+++ src/app/layout/content/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
- 
+import { RouterLink, RouterModule } from '@angular/router';
+import { ShopCartService } from '../../../../services/shop-cart';
+
 @Component({
   selector: 'app-nav-bar',
-  imports: [MatButtonModule, MatIconModule , RouterLink],
+  imports: [MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  private readonly shoppingCartState = inject(ShopCartService).state;
+
+  readonly counter = computed(() => this.shoppingCartState()?.length || 0);
+}
