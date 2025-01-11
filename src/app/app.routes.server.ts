8b0c762f93ae332c9f5eb9d3234671f7a548581a
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2025-01-11 13:55:46 -0500

    feat: Add example pre render mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,10 +1,30 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { inject } from '@angular/core';
+import { PrerenderFallback, RenderMode, ServerRoute } from '@angular/ssr';
+import { ProductService } from './services/products';
+import { firstValueFrom } from 'rxjs';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'check-out',
     renderMode: RenderMode.Client,
   },
+  {
+    path: 'router-context',
+    renderMode: RenderMode.Client,
+  },
+  // {
+  //   path: 'products/:id',
+  //   renderMode: RenderMode.Prerender,
+  //   async getPrerenderParams() {
+  //     const productService = inject(ProductService);
+  //     const listProducts = await firstValueFrom(productService.getProducts());
+  //     const listPreRender = listProducts.data.map(({ id }) => ({
+  //       id: id.toString(),
+  //     }));
+  //     return listPreRender;
+  //   },
+  //   fallback: PrerenderFallback.Client,
+  // },
   {
     path: '**',
     renderMode: RenderMode.Server,
